7990049f1698a6273eeab44745b3e68b92250c5b
r0ngk <kevinshu81@gamil.com>
2022-04-23 09:05:59 +0100

    implement mqtt subscribers

---
+++ subscriber_sim.py
@@ -0,0 +1,35 @@
+import paho.mqtt.client as mqtt
+
+hostname = "test.mosquitto.org"  # Sandbox broker
+port = 1883  # Default port for unencrypted MQTT
+
+topic = "Bathroom/#"  # Wildcard character '#' indicates all sub-topics (e.g. PC000/test, PC000/sensor/temperature, etc.)
+
+def on_connect(client, userdata, flags, rc):
+	# Successful connection is '0'
+	print("Connection result: " + str(rc))
+	if rc == 0:
+		# Subscribe to topics
+		client.subscribe(topic)
+
+def on_message(client, userdata, message):
+	print("Received message on %s: %s" % 
+		(message.topic, message.payload.decode("utf-8")))
+
+def on_disconnect(client, userdata, rc):
+	if rc != 0:
+		print("Disconnected unexpectedly")
+
+# Initialize client instance
+client = mqtt.Client()
+
+# Bind events to functions
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_disconnect = on_disconnect
+
+# Connect to the specified broker
+client.connect(hostname, port=port)
+
+# Network loop runs in the background to listen to the events
+client.loop_forever()
