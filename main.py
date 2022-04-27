67c09214fef02cd28ba82e0a9cdcd609f7ffe889
r0ngk <kevinshu81@gamil.com>
2022-04-27 08:58:12 +0100

    implement user pref and records

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ state = state_init
 
 roomTemp = 0
 roomHumid = 0
-showerTime = 60
-waterTemp = 40
+AVG_WATER_TEMP = 0
+
 time_prev = time.time()
 time_heaterStart = 0
 sys_start = True
@@ -69,6 +69,7 @@ def init(client):
     global state
     global time_prev
     global shower_start
+    global time_heaterStart
     # measure data when system start
     if sys_start == True:
         state = state_measure
@@ -85,7 +86,7 @@ def init(client):
         state = state_measure
         time_prev = time_now
     # check if heater should be closed
-    if time_heaterStart != 0 and time_now - time_heaterStart > 20+30:
+    if time_heaterStart != 0 and time_now - time_heaterStart > 20+30: # wait time + pre-start time
         publish(client, "Bathroom/heater", {"power_on" : False})
         time_heaterStart = 0
     
@@ -98,7 +99,7 @@ def measure(client):
     global state
     global fan_on
 
-    roomTemp = sense.get_temperature
+    roomTemp = sense.get_temperature()
     roomHumid = sense.get_humidity()
     topic = "Bathroom/ventilation"
     # turn on ventaliation if humidity is high
@@ -112,12 +113,12 @@ def measure(client):
         
     state = state_init
 
-
-
-def shower(client):
+def shower(client, showerTime, waterTemp):
     global shower_end
     global state
     global fan_on
+    
+    data = {'room temperature': roomTemp, 'room humidity': roomHumid}
     # turn on ventilation
     if not fan_on:
         publish(client, "Bathroom/Ventilation", {"power_on" : True})
@@ -126,7 +127,7 @@ def shower(client):
     time_start = time.time()
     # initialize thread and music player
     timer = timerThread(1, "timer", showerTime, time_start, client)
-    tempAdjust = waterTempThread(1, "tempAjust", client)
+    tempAdjust = waterTempThread(1, "tempAjust", client, waterTemp)
     player = vlc.MediaPlayer("song.mp3")
     # start threads
     player.play()
@@ -140,8 +141,23 @@ def shower(client):
     tempAdjust.join()
     # consume the token
     shower_end = False
+    # consume the data
+    data['average water temperature'] = AVG_WATER_TEMP
+    data['duration'] = time.time() - time_start
+    data['time'] = time_start
+    write_json(data)
     state = state_init
-    
+
+
+
+def write_json(data):
+    with open('data.json', 'r+') as file:
+        file_data = json.load(file)
+        file_data['records'].append(data)
+        file.seek(0)
+        json.dump(file_data, file, indent = 4)
+
+
 
 class timerThread (threading.Thread):
     def __init__(self, threadID, name, time2count, time_start, client):
@@ -153,9 +169,10 @@ class timerThread (threading.Thread):
         self.heaterSetted = False
         self.client = client
         self.topic = "Bathroom/heater"
-
+        
     def run(self):
         global shower_end
+
         while not shower_end:
             time_left = int(self.time2count - (time.time() - self.time_start))
             time_scaled = time_left / self.time2count * 64
@@ -165,7 +182,7 @@ class timerThread (threading.Thread):
             time.sleep(0.5)
             if time_left < 0:
                 break
-            if time_left  == 30 and not self.heaterSetted:
+            if (time_left  == 30 and not self.heaterSetted) and roomTemp < 25:
                 publish(self.client, self.topic, {'power_on' : True})
                 global time_heaterStart
                 time_heaterStart = time.time()
@@ -180,22 +197,28 @@ class timerThread (threading.Thread):
 
 
 class waterTempThread (threading.Thread):
-    def __init__(self, threadID, name, client):
+    def __init__(self, threadID, name, client, temp):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.client = client
         self.topic = "Bathroom/water-temp"
+        self.target_temp = temp
 
     def run(self):
         global shower_end
+        global AVG_WATER_TEMP
+        
+        avg_water_temp = sense.get_temperature() 
         while not shower_end:
-            if sense.get_temperature() < waterTemp - 3:
+            water_temp = sense.get_temperature()
+            if water_temp < self.target_temp - 3:
                 publish(self.client, self.topic, {'water_temp2low': True})
-            elif sense.get_temperature() > waterTemp + 3:
+            elif water_temp > self.target_temp + 3:
                 publish(self.client, self.topic, {'water_temp2high' : True})
-
+            avg_water_temp = avg_water_temp*3/4 + water_temp/4 # pesudo average
             time.sleep(3)
+        AVG_WATER_TEMP = avg_water_temp
 
 
 class subscribeThread(threading.Thread):
@@ -209,18 +232,28 @@ class subscribeThread(threading.Thread):
         self.client.loop_forever()
         
 def run():
-    
-    client = createMqttClient() # create client object
+    # load user preference
+    with open('pref.json') as json_file:
+        pref = json.load(json_file)
+        showerTime = pref['showerTime']
+        waterTemp = pref['waterTemp']
+    '''
+    pref = {'waterTemp':40,'showerTime':60}
+    with open('pref.json', 'w') as outfile:
+        json.dump(pref, outfile)
+    '''
+    # connect to mqtt publisher and listener
+    client = createMqttClient() 
     listener = subscribeThread(1,"listener",client)
     listener.start()
-    
+    # fsm logic
     while True:
         if state == 0:
             init(client)
         elif state == 1:
             measure(client)
         elif state == 2:
-            shower(client)
+            shower(client, showerTime, waterTemp)
 
 
 if __name__ == '__main__':
